c9768c243a306573d2cb806135e639b5734e4a13
Anoop George <anoopgeorge418@gmail.com>
2025-08-01 17:06:57 +0530

    Projects Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+
+function App() {
+  return (
+    <>
+      <div className='flex justify-center items-center'>
+        <h1>This is a heading</h1>
+        <p>This is a Para</p>
+      </div>
+    </>
+  )
+}
+
+export default App
